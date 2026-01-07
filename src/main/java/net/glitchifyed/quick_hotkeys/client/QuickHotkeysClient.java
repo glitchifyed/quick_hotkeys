75a91c869084f6440b4f07cef84659a0e2684a0c
glitchifyed <58381421+glitchifyed@users.noreply.github.com>
2026-01-07 19:37:29 +1300

    Update to 1.21.11

--- src/main/java/net/glitchifyed/quick_hotkeys/client/QuickHotkeysClient.java
+++ src/main/java/net/glitchifyed/quick_hotkeys/client/QuickHotkeysClient.java
@@ -1,8 +1,6 @@
 package net.glitchifyed.quick_hotkeys.client;
 
-import dev.isxander.yacl3.config.v2.api.ConfigClassHandler;
 import net.fabricmc.api.ClientModInitializer;
-import net.glitchifyed.quick_hotkeys.config.QuickHotkeysConfig;
 import net.glitchifyed.quick_hotkeys.event.KeyInputHandler;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.sound.PositionedSoundInstance;
@@ -29,11 +27,11 @@ public class QuickHotkeysClient implements ClientModInitializer {
         LOGGER.info("Quick Hotkeys has been fully loaded");
     }
 
-    public static void PlaySound(SoundEvent soundEvent, float pitch, float volume) {
+    public static void playSound(SoundEvent soundEvent, float pitch, float volume) {
         CLIENT.getSoundManager().play(PositionedSoundInstance.master(soundEvent, pitch, volume));
     }
 
-    public static void PlaySound(RegistryEntry.Reference<SoundEvent> soundEvent, float pitch) {
+    public static void playSound(RegistryEntry.Reference<SoundEvent> soundEvent, float pitch) {
         CLIENT.getSoundManager().play(PositionedSoundInstance.master(soundEvent, pitch));
     }
 }
